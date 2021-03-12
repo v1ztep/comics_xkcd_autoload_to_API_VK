56d4195caba738e44cf9602956ae940dad235e8e
Vladimir <v1ztep@gmail.com>
2021-03-12 17:37:33 +0300

    Загрузил на сервер ВК

--- main.py
+++ main.py
@@ -28,18 +28,18 @@ def main():
     vk_access_token = os.getenv('VK_ACCESS_TOKEN')
     vk_group_id = os.getenv('VK_GROUP_ID')
 
-    # Path(images_folder).mkdir(parents=True, exist_ok=True)
-    # comic_number = 353
-    # url = f'https://xkcd.com/{comic_number}/info.0.json'
-    #
-    # response = get_response(url)
-    # comic_details = response.json()
-    #
-    # image_url = comic_details['img']
-    # image_name = comic_details['num']
-    # download_image(image_url, image_name)
-    # comic_comment = comic_details['alt']
-    # print(comic_comment)
+    Path(images_folder).mkdir(parents=True, exist_ok=True)
+
+    comic_number = 353
+    url = f'https://xkcd.com/{comic_number}/info.0.json'
+
+    response = get_response(url)
+    comic_details = response.json()
+
+    image_url = comic_details['img']
+    image_name = comic_details['num']
+    download_image(image_url, image_name, images_folder=images_folder)
+    comic_comment = comic_details['alt']
 
     actual_version_api = '5.130'
 
@@ -54,12 +54,26 @@ def main():
     }
 
     response = get_response(url, params=params)
-    vk_api_response = response.json()
-    print(vk_api_response)
+    vk_details = response.json()
+    print(vk_details)
+
+    upload_url = vk_details['response']['upload_url']
+
+
+    image_path = Path(f'{images_folder}/{image_name}.png')
+
+    with open(image_path, 'rb') as file:
+        files = {
+            'photo': file,
+        }
+        response = requests.post(upload_url, files=files)
+        response.raise_for_status()
+        vk_details = response.json()
 
 
     with open("description.json", "w", encoding='utf8') as file:
-        json.dump(vk_api_response, file, ensure_ascii=False, indent=4)
+        json.dump(vk_details, file, ensure_ascii=False, indent=4)
+
 
 if __name__ == '__main__':
     main()
